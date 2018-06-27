76b26182721c7665537d87e042c26796a4ea6fa4
Walter Schweitzer <wschweitzer00@gmail.com>
2018-06-27 15:22:56 -0400

    adding new skin for admin dashboard

---
+++ resources/views/dashboard/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+@include('partials.left_nav')
+
+<!-- Right Panel -->
+<div id="right-panel" class="right-panel">
+
+@include('partials.header')
+
+</div>
