6de975ea45b61a7e995bac8ac1d9cf2fe0d7292b
Walter Schweitzer <wschweitzer00@gmail.com>
2018-07-27 19:10:57 -0400

    new recommendation db layout of many to many table

--- app/recommendation/admin.py
+++ app/recommendation/admin.py
@@ -5,8 +5,5 @@ from django.http import HttpResponse
 # Register your models here.
 from .models import Preferred, NonPreferred, Position
 
-class PreferredAdmin(models.Model):
-    pass
-
 admin.site.register(Preferred)
 admin.site.register(NonPreferred)
--- app/recommendation/models.py
+++ app/recommendation/models.py
@@ -11,6 +11,7 @@ class Preferred(models.Model):
     drug_name = models.CharField(max_length=255, null=True, blank=True)
     monthly_cost = models.CharField(max_length=255, null=True, blank=True)
     notes = models.TextField(null=True, blank=True)
+    notes2 = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.drug_name
@@ -20,17 +21,10 @@ class Preferred(models.Model):
         verbose_name = 'Preferred Drug'
         verbose_name_plural = 'Preferred Drugs'
 
-''' Preference of the Preferred Drugs '''
-class Position(models.Model):
-    position_id = models.AutoField(primary_key=True)
-    preferred_drug_id = models.ForeignKey(Preferred, null=True, on_delete=models.SET_NULL)
-    position = models.IntegerField(null=True, blank=True)
-
-
 ''' Non Preferred Drugs '''
 class NonPreferred(models.Model):
     non_preferred_drug_id = models.AutoField(primary_key=True)
-    preferred = models.ManyToManyField(Preferred)
+    preferred = models.ManyToManyField(Preferred, through='Position')
     ndc = models.CharField(max_length=255, null=True, blank=True)
     drug_name = models.CharField(max_length=255, null=True, blank=True)
     monthly_cost = models.CharField(max_length=255, null=True, blank=True)
@@ -43,3 +37,10 @@ class NonPreferred(models.Model):
         ordering = ['drug_name']
         verbose_name = 'Not Preferred Drug'
         verbose_name_plural = 'Not Preferred Drugs'
+
+''' Preference of the Preferred Drugs '''
+class Position(models.Model):
+    position_id = models.AutoField(primary_key=True)
+    non_preferred_drug_id = models.ForeignKey(NonPreferred, null=True, on_delete=models.SET_NULL)
+    preferred_drug_id = models.ForeignKey(Preferred, null=True, on_delete=models.SET_NULL)
+    position = models.IntegerField(null=True, blank=True)
