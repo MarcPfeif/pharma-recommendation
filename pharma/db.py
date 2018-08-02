289bb2d12c963ced0c69a05f53c644caa9ef1fb1
Walter Schweitzer <wschweitzer00@gmail.com>
2018-08-02 11:53:45 -0400

    adding postgres sqlalchemy db class

---
+++ pharma/db.py
@@ -0,0 +1,19 @@
+''' db.py '''
+import sqlalchemy
+
+''' Db '''
+class Db:
+
+    def connect_postgres(self, user, password, db, host='localhost', port=5432):
+        url = 'postgresql://{}:{}@{}:{}/{}'
+        url = url.format(user, password, host, port, db)
+
+        # The return value of create_engine() is our connection object
+        con = sqlalchemy.create_engine(url, client_encoding='utf8')
+        meta = sqlalchemy.MetaData(bind=con, reflect=True)
+
+        return con, meta
+
+    def get_all_tables(self, meta):
+        for table in meta.tables:
+            print(table)
