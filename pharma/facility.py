c79c68cb7622142c42fccfa2f338e8b480be240a
Walter Schweitzer <wschweitzer00@gmail.com>
2018-08-09 09:14:26 -0400

    added stubbed facility file

---
+++ pharma/facility.py
@@ -0,0 +1,14 @@
+''' facility.py '''
+
+import sqlalchemy
+
+class Facility:
+
+    def __init__(self):
+        pass
+
+    def get_all_facilities(self):
+        pass
+
+    def import_facilities(self,):
+        pass
