2fce2b00325ac2cc0cbd4c97d31ed653c025d44b
Walter Schweitzer <wschweitzer00@gmail.com>
2018-08-09 09:26:15 -0400

    adding skeleton functions and notes

--- pharma/facility.py
+++ pharma/facility.py
@@ -1,14 +1,19 @@
 ''' facility.py '''
 
+from db import Db
+from mssqlserver import ConnectSqlServer
 import sqlalchemy
+
 
 class Facility:
 
     def __init__(self):
         pass
 
+    ## TODO: get all the facilities from datawarehouse
     def get_all_facilities(self):
         pass
 
+    ## TODO: import all the facilities into PSQL
     def import_facilities(self,):
         pass
